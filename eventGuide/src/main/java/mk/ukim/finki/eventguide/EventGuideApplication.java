c8a40880ef2e39b5f6f9b8fecaeea2183c99862e
ana-kostadinovska <ana.kostadinovska6@gmail.com>
2024-12-02 19:57:25 +0100

    Initial project

---
+++ eventGuide/src/main/java/mk/ukim/finki/eventguide/EventGuideApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.eventguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventGuideApplication.class, args);
+	}
+
+}
