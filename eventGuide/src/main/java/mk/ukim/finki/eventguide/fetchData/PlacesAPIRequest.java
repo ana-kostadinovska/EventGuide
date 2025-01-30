a801d66752979a912bd303fcd8b00a23fabd1b25
bojanognenov <ognenovbojan@gmail.com>
2025-01-30 23:16:40 +0100

    make multiple calls to google maps api

--- eventGuide/src/main/java/mk/ukim/finki/eventguide/fetchData/PlacesAPIRequest.java
+++ eventGuide/src/main/java/mk/ukim/finki/eventguide/fetchData/PlacesAPIRequest.java
@@ -1,32 +1,61 @@
 package mk.ukim.finki.eventguide.fetchData;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PlacesAPIRequest {
     public static void main(String[] args) {
         String apiUrl = "https://places.googleapis.com/v1/places:searchNearby";
         String apiKey = ""; //todo
         StringBuilder response = new StringBuilder();
-//        karaoke?, night_club, bar, cafe, pub, wine_bar?
-//        languageCode ako treba da se dodaj i googleMapsUri
-        String requestBody = """
+        List<String> requests = new ArrayList<>();
+        String requestBody1 = """
                     {
                       "includedTypes": ["night_club", "bar", "pub"],
                       "maxResultCount": 20,
                       "locationRestriction": {
                         "circle": {
                           "center": {
-                            "latitude": 41.9875179,
-                            "longitude": 21.4319176
+                            "latitude": 42.000636,
+                            "longitude": 21.413222
+                          },
+                          "radius": 3000.0
+                        }
+                      }
+                    }
+                """;
+        String requestBody2 = """
+                    {
+                      "includedTypes": ["night_club", "bar", "pub"],
+                      "maxResultCount": 20,
+                      "locationRestriction": {
+                        "circle": {
+                          "center": {
+                            "latitude": 41.992699,
+                            "longitude": 21.433445
+                          },
+                          "radius": 5000.0
+                        }
+                      }
+                    }
+                """;
+        String requestBody3 = """
+                    {
+                      "includedTypes": ["bar", "pub"],
+                      "maxResultCount": 20,
+                      "locationRestriction": {
+                        "circle": {
+                          "center": {
+                            "latitude": 41.982741,
+                            "longitude": 21.471009
                           },
                           "radius": 5000.0
                         }
@@ -34,73 +63,73 @@ public class PlacesAPIRequest {
                     }
                 """;
 
-        try {
-            URL url = new URL(apiUrl);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type", "application/json");
-            connection.setRequestProperty("X-Goog-Api-Key", apiKey);
-            connection.setRequestProperty("X-Goog-FieldMask", "places.displayName,places.formattedAddress,places.primaryType,places.nationalPhoneNumber,places.regularOpeningHours");
-            connection.setDoOutput(true);
-
-            try (OutputStream os = connection.getOutputStream()) {
-                byte[] input = requestBody.getBytes(StandardCharsets.UTF_8);
-                os.write(input, 0, input.length);
-            }
-
-            int responseCode = connection.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                try (var reader = new java.io.BufferedReader(
-                        new java.io.InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8))) {
-                    String responseLine;
-                    response = new StringBuilder();
-                    while ((responseLine = reader.readLine()) != null) {
-                        response.append(responseLine.trim());
-                    }
-                    System.out.println("Response: " + response);
-                }
-            } else {
-                System.err.println("Error: " + responseCode);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        requests.add(requestBody1);
+        requests.add(requestBody2);
+        requests.add(requestBody3);
 
         String csvFile = "places.csv";
-        StringBuilder sb = new StringBuilder();
+        try (FileWriter writer = new FileWriter(csvFile)) {
 
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode rootNode = objectMapper.readTree(response.toString());
+            writer.append("Name; Address; Type; PhoneNumber; OpeningHours\n");
 
-            try (FileWriter writer = new FileWriter(csvFile)) {
+            for (String request : requests) {
+                try {
+                    URL url = new URL(apiUrl);
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("POST");
+                    connection.setRequestProperty("Content-Type", "application/json");
+                    connection.setRequestProperty("X-Goog-Api-Key", apiKey);
+                    connection.setRequestProperty("X-Goog-FieldMask", "places.displayName,places.formattedAddress,places.primaryType,places.nationalPhoneNumber,places.regularOpeningHours");
+                    connection.setDoOutput(true);
 
-                writer.append("Name, Address, Type, PhoneNumber, OpeningHours\n");
+                    try (OutputStream os = connection.getOutputStream()) {
+                        byte[] input = request.getBytes(StandardCharsets.UTF_8);
+                        os.write(input, 0, input.length);
+                    }
 
-                JsonNode places = rootNode.get("places");
-                for (JsonNode place : places) {
-                    String name = place.get("displayName").get("text").asText();
-                    String address = place.get("formattedAddress").asText();//todo da se zema samo prviot del pred zapirkata
-                    String phoneNumber = place.has("nationalPhoneNumber") && !place.get("nationalPhoneNumber").isNull()
-                            ? place.get("nationalPhoneNumber").asText()
-                            : "N/A";
-                    String type = place.get("primaryType").asText();
-                    JsonNode workingHours = place.get("regularOpeningHours").withArray("weekdayDescriptions");
-
-                    writer.append(name).append(", ").append(address).append(", ").append(type).append(", ")
-                            .append(phoneNumber).append(", ").append(workingHours.toString()).append("\n");
-                    sb.append(name).append(", ").append(address).append(", ").append(type).append(", ")
-                            .append(phoneNumber).append(", ").append(workingHours.toString()).append("\n");
-
-                    writer.flush();
+                    int responseCode = connection.getResponseCode();
+                    if (responseCode == HttpURLConnection.HTTP_OK) {
+                        try (var reader = new java.io.BufferedReader(
+                                new java.io.InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8))) {
+                            String responseLine;
+                            response = new StringBuilder();
+                            while ((responseLine = reader.readLine()) != null) {
+                                response.append(responseLine.trim());
+                            }
+                        }
+                    } else {
+                        System.err.println("Error: " + responseCode);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
 
-            System.out.println("CSV file saved: " + csvFile);
-            System.out.println(sb.toString());
-        } catch (JsonProcessingException e) {
+                try {
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    JsonNode rootNode = objectMapper.readTree(response.toString());
+
+                    JsonNode places = rootNode.get("places");
+                    for (JsonNode place : places) {
+                        String name = place.get("displayName").get("text").asText();
+                        String address = place.get("formattedAddress").asText();
+                        String phoneNumber = place.has("nationalPhoneNumber") && !place.get("nationalPhoneNumber").isNull()
+                                ? place.get("nationalPhoneNumber").asText()
+                                : "N/A";
+                        String type = place.get("primaryType").asText();
+                        JsonNode workingHours = place.get("regularOpeningHours").withArray("weekdayDescriptions");
+
+                        writer.append(name).append("; ").append(address).append("; ").append(type).append("; ")
+                                .append(phoneNumber).append("; ").append(workingHours.toString()).append("\n");
+
+                        writer.flush();
+                    }
+
+                    System.out.println("CSV file saved: " + csvFile);
+                } catch (JsonProcessingException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
