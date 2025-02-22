b6d6730418cdacd153427371eda55f6738b77a8f
popchanovska <e.popchanovska@gmail.com>
2025-02-22 15:36:47 +0100

    mrazam bootstrap

--- EventGuideFrontend/src/main/java/mk/ukim/finki/eventguidefrontend/web/HomeController.java
+++ EventGuideFrontend/src/main/java/mk/ukim/finki/eventguidefrontend/web/HomeController.java
@@ -32,6 +32,6 @@ public class HomeController {
             }
         }
 
-        return "test-page"; // Returns the name of the HTML file without the .html extension
+        return "template"; // Returns the name of the HTML file without the .html extension
     }
 }
