995677128eae5e6d5c9f8425e480cb35044c9ac3
mateycardula <mateycardula@gmail.com>
2025-02-22 17:33:59 +0100

    adding user on request if missing

--- eventGuide/src/main/java/mk/ukim/finki/eventguide/configuration/ResourceServerConfiguration.java
+++ eventGuide/src/main/java/mk/ukim/finki/eventguide/configuration/ResourceServerConfiguration.java
@@ -1,13 +1,13 @@
 package mk.ukim.finki.eventguide.configuration;
 
+import mk.ukim.finki.eventguide.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -16,10 +16,18 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 public class ResourceServerConfiguration {
 
+
+    private final UserService userService;
+
+    public ResourceServerConfiguration(UserService userService) {
+        this.userService = userService;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(jwt->jwt.decoder(jwtDecoder())))
+                        .jwt(jwt -> jwt.decoder(jwtDecoder())
+                                .jwtAuthenticationConverter(jwtAuthenticationConverter())))
                 .authorizeHttpRequests(authz -> authz
                         .requestMatchers("/public/**").permitAll()
                         .anyRequest().authenticated())
@@ -45,5 +53,28 @@ public class ResourceServerConfiguration {
     public JwtDecoder jwtDecoder() {
         return NimbusJwtDecoder.withJwkSetUri("https://cognito-idp.eu-north-1.amazonaws.com/eu-north-1_seACFYqEI/.well-known/jwks.json")
                 .build();
+    }
+
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            checkAndAddUser(jwt);
+            return null;
+        });
+        return converter;
+    }
+
+    private void checkAndAddUser(Jwt jwt) {
+        String sub = jwt.getClaimAsString("sub");
+        String username =sub;
+        String name = sub;
+        String surname = sub;
+        String email = sub;
+
+        if (sub != null) {
+            userService.checkAndAddUser(sub, username, name, surname, email);
+        }
     }
 }
